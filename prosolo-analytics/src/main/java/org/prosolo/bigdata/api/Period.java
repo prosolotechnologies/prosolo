de64811a2ade91ea9bec2deab3d05767d506bac0
Aleksandar Terentic <aterentic@gmail.com>
2015-11-07 23:08:36 +0100

    Removed MONTH statistics.

--- prosolo-analytics/src/main/java/org/prosolo/bigdata/api/Period.java
+++ prosolo-analytics/src/main/java/org/prosolo/bigdata/api/Period.java
@@ -3,14 +3,12 @@ package org.prosolo.bigdata.api;
 import org.prosolo.bigdata.utils.DateUtil;
 
 enum Period {
-	DAY, WEEK, MONTH;
+	DAY, WEEK;
 	
 	public Long firstDayFor(long day) {
 		switch(this) {
 		case WEEK:
 			return DateUtil.getFirstDayOfWeek(day);
-		case MONTH:
-			return DateUtil.getFirstDayOfMonth(day);
 		case DAY:
 			return day;
 		default:
