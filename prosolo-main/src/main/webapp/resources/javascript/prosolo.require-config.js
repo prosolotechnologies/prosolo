80ae58ad824084cf23d494d3004acbeb4b53fb62
Aleksandar Terentic <aterentic@gmail.com>
2015-11-27 22:37:08 +0100

    BaseURL read from server.

--- prosolo-main/src/main/webapp/resources/javascript/prosolo.require-config.js
+++ prosolo-main/src/main/webapp/resources/javascript/prosolo.require-config.js
@@ -1,9 +1,9 @@
-requirejs.config({
-    "baseUrl": "/prosolo/resources/javascript",
+var require = {
+	"baseUrl": 	document.querySelector("script[src$='prosolo.require-config.js']").dataset.baseUrl,
     "paths": {
     	"jquery": 'jquery-1.11.3.min',
     	'jqueryui': 'jquery-ui-1.11.2.min',
     	'd3': 'd3.min',
     	'underscore': 'underscore-min',
     }
-});
+};
