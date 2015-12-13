3dca5d54b4a5a50695a5609f2162426192642047
Aleksandar Terentic <aterentic@gmail.com>
2015-12-13 18:38:52 +0100

    Merged new design with activity graph.

--- prosolo-main/src/main/webapp/resources/javascript/dashboard/datepicker.js
+++ prosolo-main/src/main/webapp/resources/javascript/dashboard/datepicker.js
@@ -1,35 +1,34 @@
-define(['jquery', 'jqueryui'], function($) {
+define([ 'jquery', 'jqueryui' ], function($) {
 	$.extend($.datepicker, {
 		_checkOffset : function(inst, offset, isFixed) {
-			return offset
+			return offset;
 		}
 	});
-	
+
 	return {
 		init : function(chartId, onSelect) {
 			var selector = "#" + chartId + " .dateField";
 			var options = {
-				showOn : "both",
-				buttonImage : "../resources/css/prosolo-theme/images/calendar18x15.png",
-				buttonImageOnly : true,
-				dateFormat : "dd.mm.yy.",
-				changeMonth : true,
-				changeYear : true,
-				showOtherMonths : true,
-				selectOtherMonths : true,
-				onSelect : onSelect
-			}
+					showOn : "both",
+					buttonImage : "../resources/css/prosolo-theme/images/calendar18x15.png",
+					buttonImageOnly : true,
+					dateFormat : "dd.mm.yy.",
+					changeMonth : true,
+					changeYear : true,
+					showOtherMonths : true,
+					selectOtherMonths : true,
+					onSelect : onSelect
+			};
 			$(selector).datepicker(options);
 			$(selector).datepicker('setDate', new Date());
 		},
-		align : function (from, to, period) {
-			
+		align : function(from, to, period) {
 			function addDays(current, days) {
-			    var result = new Date(current.valueOf());
-			    result.setDate(result.getDate() + days);
-			    return result;
+				var result = new Date(current.valueOf());
+				result.setDate(result.getDate() + days);
+				return result;
 			}
-			
+
 			var days = 0;
 			if (period == "DAY") {
 				days = -2;
@@ -38,6 +37,7 @@ define(['jquery', 'jqueryui'], function($) {
 			} else if (period == "MONTH") {
 				days = -60;
 			}
+
 			var date = $(to).datepicker("getDate");
 			$(from).datepicker('option', 'maxDate', addDays(date, days));
 		}
