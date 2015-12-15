8fcf18cf431a688c06df4bc5d2842f41ef392699
Aleksandar Terentic <aterentic@gmail.com>
2015-12-15 23:07:51 +0100

    Added load test generator for twitter hashtags.

---
+++ prosolo-analytics/src/test/java/org/prosolo/bigdata/dal/impl/TwitterHashtagDailyCountTest.java
@@ -0,0 +1,34 @@
+package org.prosolo.bigdata.dal.impl;
+
+import java.util.UUID;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.prosolo.bigdata.dal.cassandra.impl.TwitterHashtagStatisticsDBManagerImpl;
+import org.prosolo.bigdata.utils.DateUtil;
+
+public class TwitterHashtagDailyCountTest {
+
+	@Ignore
+	@Test
+	public void generateEntries() {
+		// Generates 10000 hashtag counts for last two weeks.
+		TwitterHashtagStatisticsDBManagerImpl manager = new TwitterHashtagStatisticsDBManagerImpl();
+		long to = DateUtil.getDaysSinceEpoch();
+		long from = to - 15;
+
+		for (int hashtagIndex = 0; hashtagIndex < 10000; hashtagIndex++) {
+			String hashtag = UUID.randomUUID().toString();
+			for (long day = from; day <= to; day++) {
+				for (int count = random(1, 100); count > 0; count--) {
+					manager.updateTwitterHashtagDailyCount(hashtag, day);
+				}
+			}
+		}
+	}
+
+	private int random(int lo, int hi) {
+		return (int) (lo + Math.round(Math.random() * (hi - lo)));
+	}
+
+}
