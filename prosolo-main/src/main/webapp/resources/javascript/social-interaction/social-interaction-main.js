d61e0acb98955ed126b2649053e76e15a29cba2f
Aleksandar Terentic <aterentic@gmail.com>
2015-12-20 08:50:37 +0100

    Formated social interaction scripts.

--- prosolo-main/src/main/webapp/resources/javascript/social-interaction/social-interaction-main.js
+++ prosolo-main/src/main/webapp/resources/javascript/social-interaction/social-interaction-main.js
@@ -1,14 +1,14 @@
 require([ 'social-interaction/social-interaction-graph' ], function(graph) {
-    var root = document.getElementById("social-interaction");
-    
-    graph.load({
-	host: root.dataset.api,
-	studentId : root.dataset.studentId,
-        width : 800,
-        height : 600,
-        links : 100,
-        selector : "#graph",
-        charge : -300,
-        distance : 50
-    });
+	var root = document.getElementById("social-interaction");
+
+	graph.load({
+		host: root.dataset.api,
+		studentId : root.dataset.studentId,
+		width : 800,
+		height : 600,
+		links : 100,
+		selector : "#graph",
+		charge : -300,
+		distance : 50
+	});
 });
