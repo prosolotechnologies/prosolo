af1bc1546ad9f2900465fd3d85fd54806b3b1748
jeremycod <zoran.jeremic@gmail.com>
2016-04-19 22:11:17 -0700

    Finished social interaction graph node click

--- prosolo-main/src/main/webapp/resources/javascript/social-interaction/social-interaction-main.js
+++ prosolo-main/src/main/webapp/resources/javascript/social-interaction/social-interaction-main.js
@@ -28,14 +28,111 @@ $(function () {
 			{ lower: 85, upper: 100, type: "onezerozero" }	
 		],
 		onNodeClick : function(student) {
+			$("#social-interactions-nonodeselected").hide();
+			$("#social-interactions-selectedstudent").show();
+			$("#social-interactions-by-type-panel").show();
+			$("#social-interactions-by-peer-panel").show();
 			$("#social-interactions-selected-id").text(student.id);
 			$("#social-interactions-selected-name").text(student.name);
 			$("#social-interactions-selected-cluster").text(student.cluster);
 			$("#social-interactions-selected-avatar").attr("src", student.avatar);
 			$("#social-interactions-selected-avatar").show();
+			initializeDataForSelectedStudent(student,root.dataset.courseId);
 		},
 		noResultsMessage: "No results found for given parameters.",
 		systemNotAvailableMessage: "System is not available."
 	});
+var getStudentsData=function(peers){
+	return $.ajax({
+		url : "http://" + root.dataset.api + "/api/social/interactions/data",
+		data : {"students" : peers},
+		type : "GET",
+		crossDomain: true,
+		dataType: 'json'
+	});
+}
+	var initializeDataForSelectedStudent=function(student, courseid){
+		$.ajax({
+			url : "http://" + root.dataset.api + "/api/social/interactions/interactionsbypeers/"+courseid+"/"+student.id,
+			//data : {"students" : part},
+			type : "GET",
+			crossDomain: true,
+			dataType: 'json'
+		}).done(function(data){
+			for(var i=0;i<data[0].interactions.length;i++){
+				data[0].interactions[i]=JSON.parse(data[0].interactions[i]);
+			}
+			var interactions=data[0].interactions;
+			var peersinteractions={};
+			var peers=[];
+			if(interactions.length>0){
+				interactions.forEach(function(interaction){
+					var intobject={};
+					if(typeof (peersinteractions[interaction.peer])!=='undefined'){
+							intobject=peersinteractions[interaction.peer];
+					}else peers.push(interaction.peer);
+					intobject[interaction.direction]={count:interaction.count, percentage: Math.round(interaction.percentage*100)}
+					peersinteractions[interaction.peer]=intobject;
+				});
+				var studentsData=getStudentsData(peers);
+					$.when(
+					getStudentsData(peers))
+					.then(function(studentsData){
+							$("#social-interactions-bypeer-table").remove();
+						var innerHtml="<table id='social-interactions-bypeer-table'><tr style='font-weight:bold'><td style='width:120px'>Student</td><td style='width:80px'>OUT</td><td style='width:80px'>IN</td></tr>";
+
+						for(var peerId in peersinteractions){
+							var interaction=peersinteractions[peerId];
+							var student=studentsData[peerId];
+							innerHtml=innerHtml+"<tr><td>"+student.name+"</td>"
+							if(typeof(interaction.OUT)!=='undefined')
+								innerHtml=innerHtml+"<td>"+interaction.OUT.count+"("+interaction.OUT.percentage+" %)</td>";
+							if(typeof(interaction.IN)!=='undefined')
+								innerHtml=innerHtml+"<td>"+interaction.IN.count+"("+interaction.IN.percentage+" %)"+"</td>";
+
+
+						};
+
+
+						innerHtml=innerHtml+"</table>";
+								$("#social-interactions-bypeers").append(innerHtml);
+						$("social-interactions-by-peer-panel").show();
+
+					});
+
+
+			}
+		});
+
+		$.ajax({
+			url : "http://" + root.dataset.api + "/api/social/interactions/interactionsbytype/"+courseid+"/"+student.id,
+			//data : {"students" : part},
+			type : "GET",
+			crossDomain: true,
+			dataType: 'json'
+		}).done(function(data){
+			for(var i=0;i<data[0].interactions.length;i++){
+				data[0].interactions[i]=JSON.parse(data[0].interactions[i]);
+			}
+			var interactions=data[0].interactions;
+			if(interactions.length>0){
+				$("#social-interactions-bytype-table").remove();
+				var innerHtml="<table id='social-interactions-bytype-table'><tr style='font-weight:bold'><td style='width:100px'>Type</td><td style='width:80px'>OUT</td><td style='width:80px'>IN</td></tr>";
+				interactions.forEach(function(interaction){
+						innerHtml=innerHtml+"<tr><td>"+interaction.type+"</td><td>"
+						+interaction.fromusercount+"("+Math.round(interaction.fromuserpercentage*100)+" %)</td><td>"
+						+interaction.tousercount+"("+Math.round(interaction.touserpercentage*100)+" %)"+
+						"</td>";
+
+
+				});
+				innerHtml=innerHtml+"</table>";
+				$("#social-interactions-bytype").append(innerHtml);
+				$("social-interactions-by-type-panel").show();
+
+			}
+
+		});
+	};
 
 });
