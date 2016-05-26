fc6052ce0f684660d7284bd4791c02ff8281d28b
Nikola Maric <nkl.maric@gmail.com>
2016-05-26 23:03:04 +0200

    Fix learning events file loading issue

--- prosolo-analytics/src/main/java/org/prosolo/bigdata/session/impl/LearningEventsMatcherDaoImpl.java
+++ prosolo-analytics/src/main/java/org/prosolo/bigdata/session/impl/LearningEventsMatcherDaoImpl.java
@@ -1,6 +1,8 @@
 package org.prosolo.bigdata.session.impl;
 
 import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,44 +13,51 @@ import org.json.simple.parser.JSONParser;
 import org.prosolo.bigdata.events.pojo.LogEvent;
 import org.prosolo.bigdata.session.EventMatcherDao;
 
-
 /**
  * @author Nikola Maric
  *
  */
 public class LearningEventsMatcherDaoImpl implements EventMatcherDao<LogEvent> {
-	
+
 	private static Logger logger = Logger.getLogger(LearningEventsMatcherDaoImpl.class);
 	private static List<LearningEventMatcher> matchers;
 	
 	public static class LearningEventsMatcherDaoImplHolder {
 		public static final LearningEventsMatcherDaoImpl INSTANCE = new LearningEventsMatcherDaoImpl();
 	}
+
 	public static LearningEventsMatcherDaoImpl getInstance() {
 		return LearningEventsMatcherDaoImplHolder.INSTANCE;
 	}
 
-	//fetch match rules when class is initialized (or maybe lazy initialize it upon first request?)
+	// fetch match rules when class is initialized (or maybe lazy initialize it
+	// upon first request?)
 	static {
-		//TODO move location to config file and read location from there
-		String learningEventsFile = "src/main/resources/config/learningEventsMatchRules.json";
-		String milestoneTypeFile = "src/main/resources/config/learningEventsMilestoneIds.json";
-		initializeListOfMatchers(learningEventsFile,milestoneTypeFile);
+		// TODO move location to config file and read location from there
+		String learningEventsFile = "config/learningEventsMatchRules.json";
+		String milestoneTypeFile = "config/learningEventsMilestoneIds.json";
+		initializeListOfMatchers(learningEventsFile, milestoneTypeFile);
 	}
 
 	@Override
 	public List<LearningEventMatcher> getEventMatchers() {
 		return matchers;
 	}
-	
+
 	private static void initializeListOfMatchers(String learningEventsFile, String milestoneTypesFile) {
-	try {
-			JSONArray learningEventRules = (JSONArray) new JSONParser().parse(new FileReader(learningEventsFile));
-			JSONArray milestoneTypes = (JSONArray) new JSONParser().parse(new FileReader(milestoneTypesFile));
-			//JSONArray is a raw sub type of ArrayList. Methods it inherits are raw, all their type parameter 
-			//uses are erased and reduced to Object. So no ugly casting in streams, using just foreach loop.
+		try (InputStream milestonesStream = LearningEventsMatcherDaoImpl.class.getClassLoader()
+				.getResourceAsStream(milestoneTypesFile);
+				InputStream learningEventsStreamStream = LearningEventsMatcherDaoImpl.class.getClassLoader()
+						.getResourceAsStream(learningEventsFile)) {
+
+			JSONArray learningEventRules = (JSONArray) new JSONParser().parse(new InputStreamReader(learningEventsStreamStream));
+			JSONArray milestoneTypes = (JSONArray) new JSONParser().parse(new InputStreamReader(milestonesStream));
+			// JSONArray is a raw sub type of ArrayList. Methods it inherits are
+			// raw, all their type parameter
+			// uses are erased and reduced to Object. So no ugly casting in
+			// streams, using just foreach loop.
 			List<LearningEventSummary.Milestone> milestoneTypesList = new ArrayList<>(milestoneTypes.size());
-			for(Object milestoneType : milestoneTypes) {
+			for (Object milestoneType : milestoneTypes) {
 				JSONObject jsonMilestone = (JSONObject) milestoneType;
 				LearningEventSummary.Milestone milestone = new LearningEventSummary.Milestone();
 				milestone.setId(jsonMilestone.get("id").toString());
@@ -57,14 +66,14 @@ public class LearningEventsMatcherDaoImpl implements EventMatcherDao<LogEvent> {
 				milestoneTypesList.add(milestone);
 			}
 			matchers = new ArrayList<>(learningEventRules.size());
-			for(int i = 0; i < learningEventRules.size(); i++) {
-				matchers.add(LearningEventMatcher.fromJSONString(learningEventRules.get(i).toString(),
-						milestoneTypesList));
+			for (int i = 0; i < learningEventRules.size(); i++) {
+				matchers.add(
+						LearningEventMatcher.fromJSONString(learningEventRules.get(i).toString(), milestoneTypesList));
 			}
-			logger.info(String.format("Initialized %d event matchers",matchers.size()));
+			logger.info(String.format("Initialized %d event matchers", matchers.size()));
 		} catch (Exception e) {
 			logger.error("Failed to parse rules for matching learning events", e);
 		}
-	}			
-	
+	}
+
 }
