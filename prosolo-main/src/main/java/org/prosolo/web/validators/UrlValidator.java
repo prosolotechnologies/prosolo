a721342eed78b56e2e8910d22a084f4e1754674b
Stefan Vuckovic <you@example.com>
2016-06-17 16:03:02 +0200

    Encoding added urls on create-activity page

--- prosolo-main/src/main/java/org/prosolo/web/validators/UrlValidator.java
+++ prosolo-main/src/main/java/org/prosolo/web/validators/UrlValidator.java
@@ -1,5 +1,6 @@
 package org.prosolo.web.validators;
 
+import org.prosolo.common.util.string.StringUtil;
 import org.prosolo.web.validator.Validator;
 
 public class UrlValidator extends Validator {
@@ -12,7 +13,14 @@ public class UrlValidator extends Validator {
 	protected Object validate(Object obj, String message) throws Exception {
 		org.apache.commons.validator.routines.UrlValidator urlValidator = 
 				new org.apache.commons.validator.routines.UrlValidator();
-		boolean valid = urlValidator.isValid(obj.toString());
+		//String decodeUrl = URLDecoder.decode(obj.toString(),"UTF-8");
+		//System.out.println("DECODED URL " + decodeUrl);
+		//String decodedWithUri = new URI(obj.toString()).getPath();
+		//System.out.println("DECODED WITH URI " + decodedWithUri);
+		//URL urlString = new URL(decodeUrl);
+		//URI uri = new URI(urlString.getProtocol(), urlString.getUserInfo(), urlString.getHost(), urlString.getPort(), urlString.getPath(), urlString.getQuery(), urlString.getRef());
+		//System.out.println("URI " + uri.toString());
+		boolean valid = urlValidator.isValid(StringUtil.encodeUrl(obj.toString()));
 		if(valid) {
 			return obj;
 		}
