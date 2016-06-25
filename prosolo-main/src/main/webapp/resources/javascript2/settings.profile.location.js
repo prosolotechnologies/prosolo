fef16b5c43aa6f4f7759145e8a027df7f7ef354f
Musa Paljos <paljos.musa@gmail.com>
2016-06-25 16:19:00 +0200

    PRS-2125 User should set location on the Account Settings page
    Implemented functionality of setting location.

---
+++ prosolo-main/src/main/webapp/resources/javascript2/settings.profile.location.js
@@ -0,0 +1,106 @@
+function initializeMap(locationName, lat, lng) {
+	var mapOptions = {
+		center: new google.maps.LatLng(lat, lng),
+		zoom: 13,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map($('.location .map-canvas')[0], mapOptions);
+	var latitude = $('.latitude');
+	var longitude = $('.longitude');
+	
+	var input = /** @type {HTMLInputElement} */$('.location .searchLocationTextField')[0];
+	var autocomplete = new google.maps.places.Autocomplete(input);
+	
+	autocomplete.bindTo('bounds', map);
+	
+	var infowindow = new google.maps.InfoWindow();
+	var marker = new google.maps.Marker({
+		position: new google.maps.LatLng(lat, lng),
+		map: map
+	});
+	
+	infowindow.setContent('<strong>'+locationName+'</strong>');
+	infowindow.open(map, marker);
+	        
+	//if (lat == '' && lng == '') {
+	//	locateMe();
+	//}
+	
+	function setMarkerAndInfoWindow(place) {
+		marker.setPosition(place.geometry.location);
+		marker.setVisible(true);
+		
+		var address = '';
+		if (place.address_components) {
+			address = [
+				(place.address_components[0] && place.address_components[0].short_name || ''),
+				(place.address_components[1] && place.address_components[1].short_name || ''),
+				(place.address_components[2] && place.address_components[2].short_name || '')
+			].join(' ');
+		}
+		
+		infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+		infowindow.open(map, marker);
+	}
+	
+	// listener for place changed in search input field
+	google.maps.event.addListener(autocomplete, 'place_changed', function() {
+		infowindow.close();
+		marker.setVisible(false);
+		input.className = '';
+		var place = autocomplete.getPlace();
+		if (!place.geometry) {
+			// Inform the user that the place was not found and return.
+			input.className = 'notfound';
+			return;
+		}
+		
+		// If the place has a geometry, then present it on a map.
+		if (place.geometry.viewport) {
+			map.fitBounds(place.geometry.viewport);
+		} else {
+			map.setCenter(place.geometry.location);
+			map.setZoom(17);  // Why 17? Because it looks good.
+		}
+		         
+		setMarkerAndInfoWindow(place);
+		latitude.val(place.geometry.location.lat());
+		longitude.val(place.geometry.location.lng());
+	});
+}
+
+var initialLocation;
+function locateMe() {
+	var browserSupportFlag =  new Boolean();
+	
+	var myOptions = {
+		zoom: 6,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map($('.location .map-canvas')[0], myOptions);
+	
+	// Try W3C Geolocation (Preferred)
+	if (navigator.geolocation) {
+		browserSupportFlag = true;
+		navigator.geolocation.getCurrentPosition(function(position) {
+			initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+			map.setCenter(initialLocation);
+		}, function() {
+			handleNoGeolocation(browserSupportFlag);
+		});
+	}
+	// Browser doesn't support Geolocation
+	else {
+		browserSupportFlag = false;
+		handleNoGeolocation(browserSupportFlag);
+	}
+
+	function handleNoGeolocation(errorFlag) {
+		if (errorFlag == true) {
+			alert("Geolocation service failed.");
+		} else {
+			alert("Your browser doesn't support geolocation.");
+		}
+		map.setCenter(initialLocation);
+	}
+}
