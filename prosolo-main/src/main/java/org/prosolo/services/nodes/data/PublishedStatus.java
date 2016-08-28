34ee69af07beeba18e41929016d126eec3497640
Nikola Maric <nkl.maric@gmail.com>
2016-08-28 18:20:29 +0200

    added datetimrpicker to UI

--- prosolo-main/src/main/java/org/prosolo/services/nodes/data/PublishedStatus.java
+++ prosolo-main/src/main/java/org/prosolo/services/nodes/data/PublishedStatus.java
@@ -1,7 +1,7 @@
 package org.prosolo.services.nodes.data;
 
 public enum PublishedStatus {
-	DRAFT("Draft"), PUBLISHED("Published"),;
+	DRAFT("Draft"), PUBLISHED("Published"), SCHEDULED("Scheduled");
 	
 	private String label;
 
