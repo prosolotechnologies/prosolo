32475a49200a688e25f6fef9657e22707769c020
Nikola Milikic <nikola.milikic@gmail.com>
2016-09-05 19:36:29 +0200

    Fixed NPE when creating an activity PRS-2306 #time 5m #close When formatting database, email notifier is disabled for the process of initial database population.

--- prosolo-main/src/main/java/org/prosolo/services/nodes/data/ActivityData.java
+++ prosolo-main/src/main/java/org/prosolo/services/nodes/data/ActivityData.java
@@ -24,7 +24,7 @@ public class ActivityData extends StandardObservable implements Serializable {
 	private boolean enrolled;
 	private boolean completed;
 	private ActivityResultData resultData;
-	private String maxPointsString; // needed because field can also be empty on the html page
+	private String maxPointsString = ""; // needed because field can also be empty on the html page
 	
 	private int order;
 	private int durationHours;
