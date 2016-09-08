32dfe92a1bd623f78c3d889451fba5fa4ba690d6
Nikola Milikic <nikola.milikic@gmail.com>
2016-09-08 23:40:36 +0200

    Added different twitter4j properties for 'n' namespace. Fixing URL in TwitterBean.

--- prosolo-main/src/main/java/org/prosolo/web/settings/TwitterBean.java
+++ prosolo-main/src/main/java/org/prosolo/web/settings/TwitterBean.java
@@ -11,6 +11,7 @@ import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
+import org.prosolo.common.config.CommonSettings;
 import org.prosolo.common.domainmodel.user.oauth.OauthAccessToken;
 import org.prosolo.common.domainmodel.user.socialNetworks.ServiceType;
 import org.prosolo.common.domainmodel.user.socialNetworks.SocialNetworkAccount;
@@ -20,6 +21,7 @@ import org.prosolo.services.nodes.SocialNetworksManager;
 import org.prosolo.services.twitter.TwitterApiManager;
 import org.prosolo.services.twitter.UserOauthTokensManager;
 import org.prosolo.web.LoggedUserBean;
+import org.prosolo.web.util.page.PageSection;
 import org.prosolo.web.util.page.PageUtil;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -95,7 +97,10 @@ public class TwitterBean implements Serializable {
 					analyticalServiceCollector.updateTwitterUser(loggedUser.getUserId(), true);
 					
 					try {
-						String settingsUrl = PageUtil.getSectionForView().getPrefix() + "/settings";
+						String domain = CommonSettings.getInstance().config.appConfig.domain;
+						String pageSection = parameterMap.get("section");
+						
+						String settingsUrl = domain.substring(0,  domain.length()-1) + PageSection.valueOf(pageSection).getPrefix() + "/settings";
 						externalContext.redirect(settingsUrl);
 						redirected = true;
 					} catch (IOException e) {
@@ -144,14 +149,14 @@ public class TwitterBean implements Serializable {
 		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
 		externalContext.redirect(url);
 	}
-
+	
 	public String getCallbackUrl(HttpServletRequest request) {
 		String host = request.getServerName();
 		int portNumber = request.getServerPort();
 		String port = null;
 		port = (portNumber != 80) ? (":" + portNumber) : "";
 		String app = request.getContextPath();
-		String publicLink = "http://" + host + port + app + "/settings/twitterOAuth?section=" + PageUtil.getSectionForView();
+		String publicLink = CommonSettings.getInstance().config.appConfig.domain + "settings/twitterOAuth?section=" + PageUtil.getSectionForView();
 		return publicLink;
 	}
 
