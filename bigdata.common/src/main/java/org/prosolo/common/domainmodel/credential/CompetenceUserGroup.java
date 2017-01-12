c00fe8d22ba42d22d127e7cd64815abff093afbe
Stefan Vuckovic <you@example.com>
2017-01-12 16:38:36 +0100

    Adding db constraint that prevents adding the same user group to one resource more than once. Adding small ui changes - resourceVisibility component.

--- bigdata.common/src/main/java/org/prosolo/common/domainmodel/credential/CompetenceUserGroup.java
+++ bigdata.common/src/main/java/org/prosolo/common/domainmodel/credential/CompetenceUserGroup.java
@@ -4,13 +4,17 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import org.prosolo.common.domainmodel.general.BaseEntity;
 import org.prosolo.common.domainmodel.user.UserGroup;
 import org.prosolo.common.domainmodel.user.UserGroupPrivilege;
 
 @Entity
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"competence", "user_group"})})
 public class CompetenceUserGroup extends BaseEntity {
 
 	private static final long serialVersionUID = -2117827525725237618L;
@@ -28,6 +32,7 @@ public class CompetenceUserGroup extends BaseEntity {
 		this.competence = competence;
 	}
 	
+	@JoinColumn(name = "user_group")
 	@ManyToOne(fetch = FetchType.LAZY, optional = false)
 	public UserGroup getUserGroup() {
 		return userGroup;
