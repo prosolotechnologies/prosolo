96bbeefed5285ecbd6ee13e9c4562ede8a4ff3cf
Stefan Vuckovic <you@example.com>
2017-01-15 18:47:21 +0100

    Adding small changes to learning resource status names

--- prosolo-main/src/main/java/org/prosolo/services/nodes/data/PublishedStatus.java
+++ prosolo-main/src/main/java/org/prosolo/services/nodes/data/PublishedStatus.java
@@ -1,10 +1,10 @@
 package org.prosolo.services.nodes.data;
 
 public enum PublishedStatus {
-	PUBLISHED("Published"),
-	UNPUBLISH("Un-Published"),
+	PUBLISHED("Publish"),
+	UNPUBLISH("Unpublish"),
 	SCHEDULED_PUBLISH("Scheduled Publish"),
-	SCHEDULED_UNPUBLISH("Scheduled Un-Publish");
+	SCHEDULED_UNPUBLISH("Scheduled Unpublish");
 	
 	private String label;
 
