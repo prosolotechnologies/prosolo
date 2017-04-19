e4b0125dac6029b41324eb5d017eea0cdbf1893d
bojan12345 <bojantrifkovic92@gmail.com>
2017-04-19 14:51:21 +0200

    Add Difficulty Level Validator class

---
+++ prosolo-main/src/main/java/org/prosolo/web/courses/validator/DifficultyLevelValidator.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package org.prosolo.web.courses.validator;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Bojan
+ *
+ * Apr 18, 2017
+ */
+
+@Component
+@Scope("request")
+@FacesValidator("difficultyLevelValidator")
+public class DifficultyLevelValidator implements Validator {
+
+	@Override
+	public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+		
+		Integer selectedValue = (Integer)value;
+		
+		if(selectedValue == -1){
+			FacesMessage msg = new FacesMessage("Difficulty level needs to be set");
+			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+			context.addMessage("createActivity:formMain:difficultyLevelSelect", msg);
+			throw new ValidatorException(msg);
+		}
+	}
+
+}
