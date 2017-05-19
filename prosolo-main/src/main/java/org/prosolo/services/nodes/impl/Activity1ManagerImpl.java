c078af88e62e55968c730f63f398b441ae5cc0ff
Nikola Milikic <nikola.milikic@gmail.com>
2017-05-19 10:54:58 +0200

    PRS-2577 #time 1h Fixing bug with posting activity discussion comment on the activity responses page.

--- prosolo-main/src/main/java/org/prosolo/services/nodes/impl/Activity1ManagerImpl.java
+++ prosolo-main/src/main/java/org/prosolo/services/nodes/impl/Activity1ManagerImpl.java
@@ -2138,8 +2138,15 @@ public class Activity1ManagerImpl extends AbstractManagerImpl implements Activit
 					
 					if (returnAssessmentData) {
 						BigInteger assessmentId = (BigInteger) row[9];
+						
+						ActivityAssessmentData ad = new ActivityAssessmentData();
+						ad.setTargetActivityId(tActId);
+						ad.setUserId(userId);
+						ad.setActivityId(actId);
+						ad.setCompetenceId(compId);
+						ad.setCredentialId(credId);
+						
 						if(assessmentId != null) {
-							ActivityAssessmentData ad = new ActivityAssessmentData();
 							ad.setEncodedDiscussionId(idEncoder.encodeId(assessmentId.longValue()));
 							ad.setNumberOfMessages(((BigInteger) row[10]).intValue());
 							ad.setAllRead(((Character) row[11]).charValue() == 'T');
@@ -2156,7 +2163,6 @@ public class Activity1ManagerImpl extends AbstractManagerImpl implements Activit
 							ard.setAssessment(ad);							
 						} else {
 							// there is no activity assessment created yet
-							ActivityAssessmentData ad = new ActivityAssessmentData();
 							GradeData gd = new GradeData();
 							gd.setMinGrade(0);
 							gd.setMaxGrade((Integer) row[12]);
