133f75d8ed9599d56790108c44222b2caa06a656
Stefan Vuckovic <stefan.vuckovic10@gmail.com>
2017-06-08 14:57:06 +0200

    Fix url bug. Fix tab parameter issue on pages for editing credential and competence.

--- prosolo-main/src/main/java/org/prosolo/core/spring/security/SpringSecurityConfig.java
+++ prosolo-main/src/main/java/org/prosolo/core/spring/security/SpringSecurityConfig.java
@@ -200,7 +200,6 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 		   .antMatchers("/manage/settings").hasAnyAuthority("BASIC.INSTRUCTOR.ACCESS", "BASIC.MANAGER.ACCESS")
 		   //manage competence
 		   .antMatchers("/manage/competences/*/edit").hasAuthority("COURSE.CREATE")
-		   .antMatchers("/manage/credentials/*/competences/new").hasAuthority("COURSE.CREATE")
 		   .antMatchers("/manage/competences/new").hasAuthority("COURSE.CREATE")
 		   .antMatchers("/manage/competences/*/tools").hasAuthority("BASIC.MANAGER.ACCESS")
 		   .antMatchers("/manage/competences/*/visibility").hasAnyAuthority("BASIC.INSTRUCTOR.ACCESS", "BASIC.MANAGER.ACCESS")
