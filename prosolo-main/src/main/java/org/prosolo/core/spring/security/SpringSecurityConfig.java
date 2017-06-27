dbe02e3c925e623b4f5569872d7734d4e80fc704
bojan12345 <bojantrifkovic92@gmail.com>
2017-06-27 10:46:15 +0200

    Remove unnecessary mapping

--- prosolo-main/src/main/java/org/prosolo/core/spring/security/SpringSecurityConfig.java
+++ prosolo-main/src/main/java/org/prosolo/core/spring/security/SpringSecurityConfig.java
@@ -276,7 +276,6 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 		   .antMatchers("/admin/organizations/new").hasAuthority("ADMINS.VIEW")
            .antMatchers("/admin/admins/*/edit").hasAuthority("ADMINS.VIEW")
            .antMatchers("/admin/admins/*/edit/password").hasAuthority("ADMINS.VIEW")
-		   .antMatchers("/admin/admins/new").hasAuthority("ADMINS.VIEW")
 		   .antMatchers("/admin/organizations").hasAuthority("ADMINS.VIEW")
 		  
 		   .antMatchers("/manage/**").denyAll()
