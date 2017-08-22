55181f8979d9e0a7427ba5836d12f1f41f281ec2
Bojan Trifkovic <bojantrifkovic92@gmail.com>
2017-08-22 23:57:46 +0200

    Add new domain clases,fix sql script

--- bigdata.common/src/main/java/org/prosolo/common/domainmodel/rubric/Rubric.java
+++ bigdata.common/src/main/java/org/prosolo/common/domainmodel/rubric/Rubric.java
@@ -1,9 +1,12 @@
 package org.prosolo.common.domainmodel.rubric;
 
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
 import org.prosolo.common.domainmodel.general.BaseEntity;
 import org.prosolo.common.domainmodel.user.User;
 
 import javax.persistence.*;
+import java.util.List;
 
 /**
  * @author Bojan Trifkovic
@@ -16,15 +19,34 @@ import javax.persistence.*;
 public class Rubric extends BaseEntity {
 
     private User creator;
+    private List<Category> categories;
+    private List<Level> levels;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "creator")
+    @OneToOne(fetch = FetchType.LAZY)
     public User getCreator() {
         return creator;
     }
 
     public void setCreator(User creator) {
         this.creator = creator;
+    }
+
+    @OneToMany(mappedBy = "rubric", cascade = CascadeType.REMOVE)
+    public List<Level> getLevels() {
+        return levels;
+    }
+
+    public void setLevels(List<Level> levels) {
+        this.levels = levels;
+    }
+
+    @OneToMany(mappedBy = "rubric", cascade = CascadeType.REMOVE)
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
     }
 
 }
