a97734c113858f2c29ae8ab6031b866465bd110a
Bojan Trifkovic <bojantrifkovic92@gmail.com>
2017-08-24 12:43:59 +0200

    Remove rubrics list from organization

--- bigdata.common/src/main/java/org/prosolo/common/domainmodel/organization/Organization.java
+++ bigdata.common/src/main/java/org/prosolo/common/domainmodel/organization/Organization.java
@@ -24,17 +24,16 @@ public class Organization extends BaseEntity {
 
 	private List<User> users;
 	private List<Unit> units;
-	private List<Rubric> rubrics;
-	
+
 	@OneToMany(mappedBy = "organization")
 	public List<User> getUsers(){
 		return users;
 	}
-	
+
 	public void setUsers(List<User> users){
 		this.users = users;
 	}
-	
+
 	@OneToMany(mappedBy = "organization")
 	public List<Unit> getUnits(){
 		return units;
@@ -44,12 +43,4 @@ public class Organization extends BaseEntity {
 		this.units = units;
 	}
 
-	@OneToMany(mappedBy = "organization")
-	public List<Rubric> getRubrics(){
-		return rubrics;
-	}
-
-	public void setRubrics(List<Rubric> rubrics){
-		this.rubrics = rubrics;
-	}
 }
