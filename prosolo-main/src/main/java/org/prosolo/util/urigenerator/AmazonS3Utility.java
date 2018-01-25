f9612a9719c652c3e7c5cc0bb29e69e6c6941d68
Nikola Milikic <nikola.milikic@gmail.com>
2018-01-25 12:42:15 +0100

    Changed URL that is returned after a file has been uploaded. In all xhtml files (we are actively using now) removed using js scripts from the folder 'javascript'. Only files from the 'javascript2' folder is being used.

--- prosolo-main/src/main/java/org/prosolo/util/urigenerator/AmazonS3Utility.java
+++ prosolo-main/src/main/java/org/prosolo/util/urigenerator/AmazonS3Utility.java
@@ -9,19 +9,8 @@ import org.prosolo.common.config.CommonSettings;
  */
 public class AmazonS3Utility {
 	
-	private static String prefix = CommonSettings.getInstance().config.fileStore.fileStoreServiceUrl +
-									"/" + CommonSettings.getInstance().config.fileStore.fileStoreBucketName +
-									"/";
-//	private static String prefix=CommonSettings.getInstance().config.fileStore.getFilePath();
-
 	public static String createFullPathFromRelativePath(String relativeFilePath){
-		return prefix + relativeFilePath;
+		return CommonSettings.getInstance().config.fileStore.virtualHostingUrl + "/"+ relativeFilePath;
 	}
 	
-	public static String getRelativeFilePathFromFullS3Path(String fullPath) {
-		if (fullPath.startsWith(prefix)) {
-			return fullPath.replace(prefix, "");
-		}
-		return fullPath;
-	}
 }
