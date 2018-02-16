fa5dc102c454fe47e907e9c778f77809c29bb374
Stefan Vuckovic <stefan.vuckovic10@gmail.com>
2018-02-16 18:30:05 +0100

    Add changes to assessment pages according to PR comments

--- prosolo-main/src/main/java/org/prosolo/web/assessments/CredentialAssessmentBean.java
+++ prosolo-main/src/main/java/org/prosolo/web/assessments/CredentialAssessmentBean.java
@@ -377,7 +377,12 @@ public class CredentialAssessmentBean extends LearningResourceAssessmentBean imp
 
 	//LearningResourceAssessmentBean impl end
 
-	public boolean isUserAllowedToSeeRubric(GradeData gradeData) {
+	public boolean isUserAllowedToSeeRubric(GradeData gradeData, LearningResourceType resourceType) {
+		//temporary solution for credential and competency before we introduce visibility options for these resources
+		//for now students are allowed to see rubric when they are assessed
+		if (resourceType == LearningResourceType.CREDENTIAL || resourceType == LearningResourceType.COMPETENCE) {
+			return gradeData.isAssessed();
+		}
 		if (gradeData instanceof RubricGradeData) {
 			RubricGradeData rubricGradeData = (RubricGradeData) gradeData;
 			return rubricGradeData.getRubricVisibilityForStudent() != null && rubricGradeData.getRubricVisibilityForStudent() == ActivityRubricVisibility.ALWAYS
