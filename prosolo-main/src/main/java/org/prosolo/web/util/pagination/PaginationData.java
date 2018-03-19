f0dfafe7cee26175ab5577b4a8b16438c8724c33
Nikola Milikic <nikola.milikic@gmail.com>
2018-03-19 11:21:19 +0100

    Reverting default pagination limit to 10. PRS-3020

--- prosolo-main/src/main/java/org/prosolo/web/util/pagination/PaginationData.java
+++ prosolo-main/src/main/java/org/prosolo/web/util/pagination/PaginationData.java
@@ -16,7 +16,7 @@ public class PaginationData {
 
 	private List<PaginationLink> links;
 
-	private static final int DEFAULT_LIMIT = 2;
+	private static final int DEFAULT_LIMIT = 10;
 	
 	public PaginationData() {
 		this(DEFAULT_LIMIT);
