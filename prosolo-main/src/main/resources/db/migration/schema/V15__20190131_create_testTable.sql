52fe3793c5a4b7f5ebd33c2801097d210d51adc3
milikicn <nikola.milikic@gmail.com>
2019-02-01 16:11:44 +0100

    Deleted migration file 'V15__20190131_create_testTable.sql' taht was commited by mistake. PRS-3541

--- prosolo-main/src/main/resources/db/migration/schema/V15__20190131_create_testTable.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE `student_profile_config`
-  DROP COLUMN `grade`,
-  DROP COLUMN `max_grade`,
-  ADD COLUMN `grade_type` varchar(255) NOT NULL DEFAULT 'NO_GRADE',
-  ADD COLUMN `points_achieved` int(11),
-  ADD COLUMN `max_points` int(11),
-  ADD COLUMN `avg_level_achieved` int(11),
-  ADD COLUMN `number_of_levels` int(11);
