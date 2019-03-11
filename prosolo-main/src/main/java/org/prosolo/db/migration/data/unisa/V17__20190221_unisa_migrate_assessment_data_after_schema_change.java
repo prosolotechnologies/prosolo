315e9d53df5e4edf8af6944022ff893e5c6af8b3
Stefan Vuckovic <stefan.vuckovic10@gmail.com>
2019-03-11 21:42:19 +0100

    Add migration for competency assessments without reference to any credential assessments PRS-3605

--- prosolo-main/src/main/java/org/prosolo/db/migration/data/unisa/V17__20190221_unisa_migrate_assessment_data_after_schema_change.java
+++ prosolo-main/src/main/java/org/prosolo/db/migration/data/unisa/V17__20190221_unisa_migrate_assessment_data_after_schema_change.java
@@ -4,6 +4,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.prosolo.db.migration.BaseMigration;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -17,6 +18,7 @@ public class V17__20190221_unisa_migrate_assessment_data_after_schema_change ext
     @Override
     protected void doMigrate(Context context) throws Exception {
         migrateWhenCompetencyToCredentialAssessmentIsOneToOne(context.getConnection());
+        migrateWhenCompetencyAssessmentIsNotConnectedToAnyCredentialAssessments(context.getConnection());
     }
 
     private void migrateWhenCompetencyToCredentialAssessmentIsOneToOne(Connection connection) throws SQLException {
@@ -35,5 +37,34 @@ public class V17__20190221_unisa_migrate_assessment_data_after_schema_change ext
                             "ca.credential_assessment = creda.id";
             statement.executeUpdate(q);
         }
+    }
+
+    private void migrateWhenCompetencyAssessmentIsNotConnectedToAnyCredentialAssessments(Connection connection) throws SQLException {
+        String sql = "UPDATE competence_assessment ca SET ca.target_credential = ? WHERE ca.id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setLong(1, 98314);
+            statement.setLong(2, 950281);
+            statement.addBatch();
+            statement.setLong(1, 360484);
+            statement.setLong(2, 754821);
+            statement.addBatch();
+            statement.setLong(1, 196609);
+            statement.setLong(2, 754811);
+            statement.addBatch();
+            statement.setLong(1, 196609);
+            statement.setLong(2, 754822);
+            statement.addBatch();
+            statement.setLong(1, 753903);
+            statement.setLong(2, 756539);
+            statement.addBatch();
+            statement.setLong(1, 98311);
+            statement.setLong(2, 950280);
+            statement.addBatch();
+            statement.executeBatch();
+        }
+    }
+
+    private void migrateWhenCompetencyAssessmentIsConnectedToSeveralCredentialAssessments(Connection connection) throws SQLException {
+
     }
 }
