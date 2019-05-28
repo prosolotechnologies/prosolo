44eaa92832592a8450cd55a6f294d2efaa56e5b8
Stefan Vuckovic <stefan.vuckovic10@gmail.com>
2019-05-28 14:53:39 +0200

    Remove unused method PRS-3643

--- prosolo-main/src/main/java/org/prosolo/web/administration/InitDataBean.java
+++ prosolo-main/src/main/java/org/prosolo/web/administration/InitDataBean.java
@@ -63,11 +63,4 @@ public class InitDataBean implements Serializable {
         }
     }
 
-    public void test() {
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 }
