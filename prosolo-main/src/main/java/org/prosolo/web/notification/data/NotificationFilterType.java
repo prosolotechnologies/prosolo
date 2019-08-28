12ee26f58c9520b39aa7d3cdf89296801cf6aa8e
Nikola Milikic <nikola.milikic@gmail.com>
2019-08-28 15:55:50 +0200

    Update notification filter labels. PRS-3797

--- prosolo-main/src/main/java/org/prosolo/web/notification/data/NotificationFilterType.java
+++ prosolo-main/src/main/java/org/prosolo/web/notification/data/NotificationFilterType.java
@@ -11,21 +11,21 @@ public enum NotificationFilterType {
 
 	Follow_User(NotificationType.Follow_User, "Followers", List.of(PageSection.STUDENT)),
 	Comment(NotificationType.Comment, "Comments", List.of(PageSection.STUDENT, PageSection.MANAGE)),
-	Comment_Like(NotificationType.Comment_Like, "Comment Likes", List.of(PageSection.STUDENT, PageSection.MANAGE)),
+	Comment_Like(NotificationType.Comment_Like, "Comment likes", List.of(PageSection.STUDENT, PageSection.MANAGE)),
 	Mention(NotificationType.Mention, "Mentions", List.of(PageSection.STUDENT, PageSection.MANAGE)),
-	Assessment_Approved(NotificationType.Assessment_Approved, "Assessments Submitted", List.of(PageSection.STUDENT)),
-	Assessment_Requested(NotificationType.Assessment_Requested, "Assessments Requested", List.of(PageSection.STUDENT, PageSection.MANAGE)),
-	Assessment_Comment(NotificationType.Assessment_Comment, "Assessment Comments", List.of(PageSection.STUDENT, PageSection.MANAGE)),
-	GradeAdded(NotificationType.GradeAdded, "Assessments Updated", List.of(PageSection.STUDENT)),
+	Assessment_Approved(NotificationType.Assessment_Approved, "Assessments submitted", List.of(PageSection.STUDENT)),
+	Assessment_Requested(NotificationType.Assessment_Requested, "Assessments requested", List.of(PageSection.STUDENT, PageSection.MANAGE)),
+	Assessment_Comment(NotificationType.Assessment_Comment, "Assessment comments", List.of(PageSection.STUDENT, PageSection.MANAGE)),
+	GradeAdded(NotificationType.GradeAdded, "Assessments received", List.of(PageSection.STUDENT)),
 	Announcement_Published(NotificationType.AnnouncementPublished, "Announcements", List.of(PageSection.STUDENT)),
-	Social_Activity_Like(NotificationType.Social_Activity_Like, "Post Likes", List.of(PageSection.STUDENT)),
+	Social_Activity_Like(NotificationType.Social_Activity_Like, "Post likes", List.of(PageSection.STUDENT)),
 	Assessment_Request_Accepted(NotificationType.ASSESSMENT_REQUEST_ACCEPTED, "Assessment requests accepted", List.of(PageSection.STUDENT)),
 	Assessment_Request_Declined(NotificationType.ASSESSMENT_REQUEST_DECLINED, "Assessment requests declined", List.of(PageSection.STUDENT)),
-	Assessor_Withdrew_From_Assessment(NotificationType.ASSESSOR_WITHDREW_FROM_ASSESSMENT, "Withdrawn assessments by assessor", List.of(PageSection.STUDENT)),
-	Assessor_Assigned_To_Assessment(NotificationType.ASSESSOR_ASSIGNED_TO_ASSESSMENT, "Assessors assigned to your assessment requests", List.of(PageSection.STUDENT)),
-	Assigned_To_Assessment_As_Assessor(NotificationType.ASSIGNED_TO_ASSESSMENT_AS_ASSESSOR, "Assigned as assessor to assessment requests", List.of(PageSection.STUDENT)),
+	Assessor_Withdrew_From_Assessment(NotificationType.ASSESSOR_WITHDREW_FROM_ASSESSMENT, "Withdrawn assessments", List.of(PageSection.STUDENT)),
+	Assessor_Assigned_To_Assessment(NotificationType.ASSESSOR_ASSIGNED_TO_ASSESSMENT, "Assessors assigned to you", List.of(PageSection.STUDENT)),
+	Assigned_To_Assessment_As_Assessor(NotificationType.ASSIGNED_TO_ASSESSMENT_AS_ASSESSOR, "You assigned as assessor", List.of(PageSection.STUDENT)),
 	Assessment_Request_Expired(NotificationType.ASSESSMENT_REQUEST_EXPIRED, "Assessment requests expired", List.of(PageSection.STUDENT)),
-	Assessment_Tokens_Number_Updated(NotificationType.ASSESSMENT_TOKENS_NUMBER_UPDATED, "Assessment tokens number updates", List.of(PageSection.STUDENT));
+	Assessment_Tokens_Number_Updated(NotificationType.ASSESSMENT_TOKENS_NUMBER_UPDATED, "Assessment tokens updates", List.of(PageSection.STUDENT));
 
 	private NotificationType notificationType;
 	private String label;
